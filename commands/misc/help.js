f76e1e5483cee002b97b8105fc87dcadae5d53ca
Dercraker <antoine.capitain@gmail.com>
2021-11-04 11:47:00 +0100

    /help a tester

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -17,7 +17,7 @@ class HelpCommand extends Command {
             },
             slash: true,
             slashOptions: [{
-                name: 'name',
+                name: 'command',
                 description: "Nom de la commande sur le quel vous voulez plus d'info ",
                 type: 'STRING',
                 required: false
@@ -26,8 +26,57 @@ class HelpCommand extends Command {
     }
 
     exec(message, args) {
-            const prefix = this.handler.prefix
-            const command = args.command
+        const prefix = this.handler.prefix
+        const command = args.command
+        if (!command) {
+            let Embed = new MessageEmbed()
+                .setAuthor(`${lang.embeds.default.author}`, "https://www.gtplanet.net/wp-content/uploads/2021/08/ForzaHorizon5_KeyArt_Horiz_RGB_Final-800x450.jpg", "https://discord.gg/BtkWVH2Kq9")
+                .setColor('#ff8000')
+                .setDescription(lang.commands.help.embed.desc)
+                .setFooter(`${lang.embeds.default.footer}`, "https://www.gtplanet.net/wp-content/uploads/2021/08/ForzaHorizon5_KeyArt_Horiz_RGB_Final-800x450.jpg")
+                .setTimestamp()
+                .addFields()
+
+            for (const category of this.handler.categories.values()) {
+                Embed.addField(
+                        `${category.id}`,
+                        `${category
+                            .filter(cmd => cmd.aliases.length > 0)
+                            .map(cmd => `\`${cmd.aliases[0]}\``)
+                            .join(', ')
+                        }`
+                )
+            }
+        
+            Embed.addField(
+                '------------',
+                `**\`${prefix}help <command>\` pour des infos sur une commande spécifique **
+                Exemples \`${prefix}help ping\`  || \`${prefix}help embed\``
+            )
+            return message.reply({embeds: [Embed]})
+        }
+        return message.channel.send(stripIndents`
+        \`\`\`makefile
+            [help : ${command.aliases[0]}] ${command.ownerOnly ? '/!\\ Fondator Only':''}
+
+            ${command.description.content}
+
+            Utilisation: ${prefix}${command.description.usage}
+            Exemples: ${prefix}${command.description.exemples.join(`, ${prefix}`)}
+
+            
+
+            ${prefix}  @${this.client.user.username} = prefixs  a utiliser avec le bot
+            <> = argument(s) optionnel(s) | {} = argument(s) obligatoire
+            Les caractères suivants -> <>, {} ne doivents pas être inclus dans les commandes
+        \`\`\`               
+        `)
+
+
+        }
+    execSlash(message, {command}) {
+
+        const prefix = this.handler.prefix
             if (!command) {
                 let Embed = new MessageEmbed()
                     .setAuthor(`${lang.embeds.default.author}`, "https://www.gtplanet.net/wp-content/uploads/2021/08/ForzaHorizon5_KeyArt_Horiz_RGB_Final-800x450.jpg", "https://discord.gg/BtkWVH2Kq9")
@@ -53,7 +102,10 @@ class HelpCommand extends Command {
                     `**\`${prefix}help <command>\` pour des infos sur une commande spécifique **
                     Exemples \`${prefix}help ping\`  || \`${prefix}help embed\``
                 )
-                return message.reply({embeds: [Embed]})
+                return message.interaction.reply({
+                    embeds: [Embed],
+                    ephemeral: true
+                }) 
             }
             return message.channel.send(stripIndents`
             \`\`\`makefile
@@ -73,12 +125,7 @@ class HelpCommand extends Command {
             `)
 
 
-        }
-    execSlash(message, {name}) {
-        message.interaction.reply({
-            content:`${name}`,
-            ephemeral: true
-        })
+        
     }          
 }
 
