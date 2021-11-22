ef61d551bf5be1fb265725d40864a2717a43d96a
ACAPITAIN <antoine.capitain@ynov.com>
2021-11-22 10:13:52 +0100

    Roll Command OK

--- commands/moderation/roll.js
+++ commands/moderation/roll.js
@@ -8,37 +8,48 @@ class RollCommand extends Command {
             aliases: ['roll'],
             category: 'Moderation',
             description: {
-                content : rollLang.desc,
+                content: rollLang.desc,
                 usage: rollLang.usage,
-                exemples: ['roll','roll 2 10']
+                exemples: ['roll', 'roll 2 10']
             },
-            args: [
-                { id: 'nbVal', type: 'number', default: 1 },
-                { id: 'plage', type: 'number', default: 15 }
-            ]
+            slash: true,
+            slashOptions: [{
+                name: 'nbval',
+                description: "nombre de valeurs a tirer",
+                type: 'NUMBER',
+                required: true
+            }, {
+                name: 'plage',
+                description: "plage sur la quelle tirer les valeurs",
+                type: 'NUMBER',
+                required: true
+            }]
         });
     }
 
-    exec(message, {nbVal, plage}) {
-        if (nbVal > plage){
-            message.delete()
-            return message.channel.send(rollLang.messageError)
-            .then(message => {
-                setTimeout(() => message.delete(),10000)
+    exec(message) {
+        return message.reply(`Merci d'utiliser cette commande avec un slash`);
+    }
+
+    execSlash(message, { nbval, plage }) {
+        if (nbval > plage) {
+            return message.interaction.reply({
+                content: rollLang.messageError,
+                ephemeral: true
             })
         }
         let result = []
         let nb
-        for (let i = 0; i < nbVal; i++){
+        for (let i = 0; i < nbval; i++) {
             do {
                 nb = Math.floor(Math.random() * (plage - 1 + 1) + 1)
             } while (result.includes(nb));
             result.push(nb)
         }
-        result.sort((a,b) => {
+        result.sort((a, b) => {
             return a - b;
         })
-        return message.reply(`Voici vos valeur${nbVal === 1 ? '':'s'}: ${result.join(',')}`);
+        return message.interaction.reply(`Voici vos valeur${nbval === 1 ? '':'s'}: ${result.join(',')}`);
     }
 }
 
