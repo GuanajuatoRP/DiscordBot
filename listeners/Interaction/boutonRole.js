95f3a23b81e00c709551baa1df091ac99117c85f
Dercraker <antoine.capitain@gmail.com>
2021-12-07 23:55:46 +0100

    clean logs

--- listeners/Interaction/boutonRole.js
+++ listeners/Interaction/boutonRole.js
@@ -1,8 +1,6 @@
 const { Listener } = require('discord-akairo')
 const appConfig = require('../../util/appConfig.json')
-const fs = require('fs');
-const { waitForDebugger } = require('inspector');
-const wait = require('util').promisify(setTimeout);
+
 
 class boutonRoleListener extends Listener {
     constructor() {
@@ -17,9 +15,9 @@ class boutonRoleListener extends Listener {
             const RoleA = interaction.guild.roles.cache.get(appConfig.Roles.GMA);
             const RoleB = interaction.guild.roles.cache.get(appConfig.Roles.GMB);
             if (interaction.message.embeds[0].fields[0].value.indexOf(interaction.member.nickname != null ? interaction.member.nickname : interaction.member.user.username) != -1){
-                console.log(interaction.member.roles.add(RoleA));
+                interaction.member.roles.add(RoleA)
             } else {
-                console.log(interaction.member.roles.add(RoleB));
+                interaction.member.roles.add(RoleB)
             }
 
         }
