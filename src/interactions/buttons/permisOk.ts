cc855897feb7f086d9f33d5a8555fc7eeb70fc92
Dercraker <antoine.capitain@gmail.com>
2022-01-24 19:55:35 +0100

    Permis embed String in Lang.json v1

--- src/interactions/buttons/permisOk.ts
+++ src/interactions/buttons/permisOk.ts
@@ -1,7 +1,9 @@
 import { Button } from "sheweny";
 import type { ShewenyClient } from "sheweny";
-import type { ButtonInteraction, GuildMember } from "discord.js";
+import type { ButtonInteraction, ColorResolvable, GuildMember, Message } from "discord.js";
 import {MessageEmbed} from "discord.js"
+import lang from "../../util/language.json"
+const interactionLang = lang.interaction.PermisOk
 // import appConfig from '../../util/appConfig.json'
 
 export class Btns extends Button {
@@ -10,12 +12,13 @@ export class Btns extends Button {
     }
 
     async execute(button: ButtonInteraction) {
+        const message = button.message as Message;
         const member = button.member as GuildMember
         let embed = new MessageEmbed() 
-            embed.setColor('DARK_GREEN')
-            embed.setTitle('Validation du permis')
-            embed.setDescription('La ville de guanajuato vous félicite pour votre permis !!')
-            embed.setFooter("Validation du permis de : {0}".format(member.user.tag))
+            embed.setColor(interactionLang.embed.color as ColorResolvable)
+            embed.setTitle(interactionLang.embed.title)
+            embed.setDescription(interactionLang.embed.description)
+            embed.setFooter(interactionLang.embed.footer.format(member.user.tag))
             embed.setTimestamp()
             embed.setThumbnail(member.displayAvatarURL())
             // TODO : Faire une rquest api pour avoir la fiche personnel de l'utilisateur 
@@ -26,6 +29,6 @@ export class Btns extends Button {
                 {name : "Points sur le nouveau permis",value: "12", inline:true}
             )
         await button.update({embeds : [embed],components : []})
-        
+        message.react('✅')
     }
 }
