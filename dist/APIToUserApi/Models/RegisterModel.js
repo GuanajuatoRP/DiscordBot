4ba8be557612b61c5b2b990b418075b8956bfdde
Dercraker <antoine.capitain@gmail.com>
2022-08-10 22:37:16 +0200

    auto dev / prod ok

--- dist/APIToUserApi/Models/RegisterModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RegisterModel = void 0;
-class RegisterModel {
-    constructor() {
-        this.discordId = '';
-    }
-}
-exports.RegisterModel = RegisterModel;
